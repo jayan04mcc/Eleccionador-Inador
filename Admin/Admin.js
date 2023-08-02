a5e14e75699c0903bf2557566994cbc80e22681b
AxelSolis93 <104176510+AxelSolis93@users.noreply.github.com>
2023-08-01 22:05:09 -0500

    Add files via upload

--- Admin/Admin.js
+++ Admin/Admin.js
@@ -1,19 +1,29 @@
-/**
- * Admin class
- */
-class Admin {
-    /**
-     * Default constructor
-     */
-    constructor() {
-    }
+import React from 'react';
 
-   
-    voterID; 
-    voterNo; 
+class Admin extends React.Component {
+  constructor(props) {
+    super(props);
 
-   
-    submitReport() {
-        // TODO implement here
-    }
+    this.state = {
+      voterID: null,
+      voterNo: null,
+    };
+  }
+
+  submitReport = () => {
+    // TODO implement here
+  }
+
+  render() {
+    return (
+      <div>
+        
+        <p>Voter ID: {this.state.voterID}</p>
+        <p>Voter No: {this.state.voterNo}</p>
+        <button onClick={this.submitReport}>Submit Report</button>
+      </div>
+    );
+  }
 }
+
+export default Admin;
