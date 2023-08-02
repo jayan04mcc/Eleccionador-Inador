fd15e655918446a1e3482ece31cb1a6365e92d13
AxelSolis93 <104176510+AxelSolis93@users.noreply.github.com>
2023-08-01 20:43:54 -0500

    Add files via upload

---
+++ CAST VOTE/CAST VOTE.js
@@ -0,0 +1,21 @@
+class CASTVOTE {
+    constructor() {
+    }
+
+    Cand_ID;
+    Voter_ID;
+    TimeStamp;
+    Attribute2;
+
+    submitVote() {
+        // TODO implement here
+    }
+
+    cancelVote() {
+        // TODO implement here
+    }
+
+    operation2() {
+        // TODO implement here
+    }
+}
