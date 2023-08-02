b6f87a52cef5eb6b9af639969e766511c5fdfcf5
AxelSolis93 <104176510+AxelSolis93@users.noreply.github.com>
2023-08-01 22:10:09 -0500

    Add files via upload

---
+++ GenerateVotation/CAST VOTE.java
@@ -0,0 +1,56 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class CAST VOTE {
+
+    /**
+     * Default constructor
+     */
+    public CAST VOTE() {
+    }
+
+    /**
+     * 
+     */
+    public void Cand_ID;
+
+    /**
+     * 
+     */
+    public void Voter_ID;
+
+    /**
+     * 
+     */
+    public void TimeStamp;
+
+    /**
+     * 
+     */
+    public void Attribute2;
+
+    /**
+     * 
+     */
+    public void Submint Vote() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Cancel Vote() {
+        // TODO implement here
+    }
+
+    /**
+     * 
+     */
+    public void Operation2() {
+        // TODO implement here
+    }
+
+}
