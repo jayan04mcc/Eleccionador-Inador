850d5d79e594796976aa4dd008b2ec7ea2c87dd3
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2023-08-02 20:58:39 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Votacion from './CAST_VOTE'
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Votacion />
+  </React.StrictMode>,
+)
