1f7767525107fbfa30cf015cae49b646a940bf5a
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2023-08-07 12:08:55 -0500

    Update main.jsx

--- GenerateVotation/main.jsx
+++ GenerateVotation/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import Votacion from './CAST_VOTE'
+//import Votacion from './CAST_VOTE'
+import Votacion from './CASTVOTE2'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Votacion />
