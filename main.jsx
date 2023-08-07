b1d2e105f9b4337613ddade56609aee675eb365a
jayan04mcc <104178976+jayan04mcc@users.noreply.github.com>
2023-08-06 23:26:55 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Votacion from './CAST_VOTE'
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Votacion />
-  </React.StrictMode>,
-)
